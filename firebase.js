dcdde670d8f02b70c25399817859bf5775518cc5
Akshay Patil <akshaypatil9714@gmail.com>
2024-09-02 17:16:33 -0400

    stored the environment variable in .env.local file

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import {Firestore, getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCT88snXD0nUvDkuVxT5idxIQ3RTrI3c9k",
-  authDomain: "inventory-management-92e42.firebaseapp.com",
-  projectId: "inventory-management-92e42",
-  storageBucket: "inventory-management-92e42.appspot.com",
-  messagingSenderId: "1048822646069",
-  appId: "1:1048822646069:web:c7dad8d45ffef21c03f87c",
-  measurementId: "G-60VZRJL19J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
